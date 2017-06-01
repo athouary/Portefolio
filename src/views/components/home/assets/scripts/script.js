8c647a49e5d715b60bf5632cd4cbbad5da847302
Emmanuel Plouvier <eplouvie@prismamedia.com>
2017-06-01 16:16:51 +0200

    [feature/evo-r2] try to fix broken HMR

--- src/views/components/home/assets/scripts/script.js
+++ src/views/components/home/assets/scripts/script.js
@@ -6,5 +6,6 @@
 import jQuery from 'vendor/jquery/dist/jquery';
 
 jQuery(document).ready( function(){
+  jQuery('.container').css('background', 'blue')
     console.log( 'Script Component/home' );
 });

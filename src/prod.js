0ff6f11e330891c95ae935bdb0964f5cb27bac29
Auxane Thouary <athouary@gmail.com>
2018-07-08 23:22:50 +0200

    chore: add ssl

--- src/prod.js
+++ src/prod.js
@@ -1,5 +1,6 @@
 // Import Express and engine for Handlebars
 import express from 'express'
+import https from 'https'
 import ExpressHandlebars from 'express-handlebars'
 import Locale from 'locale'
 
@@ -13,6 +14,12 @@ import helpers from './helpers.js'
 
 // Init Express Server
 const app = express()
+
+https.createServer({
+  key: fs.readFileSync('/home/thougjyh/ssl/certs/auxane_thouary_com_bd093_af675_1562616856_2d29acccbb4d1b392ee6e57ea155a12e.crt'),
+  cert: fs.readFileSync('/home/thougjyh/ssl/certs/auxane_thouary_com_be6bc_80223_1538853309_86d3151f69bd8fd6a4e0bb93a52be97c.crt')
+}, app).listen('passenger')
+
 const hbs = ExpressHandlebars.create({
   layoutsDir: configVars.viewsPath,
   partialsDir: configVars.componentsPath,
